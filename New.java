18e1f445c7895951880079b7619a6f7b3f7b9b34
GeninKonstantin <91433238+GeninKonstantin@users.noreply.github.com>
2022-05-29 14:57:21 +0300

    Create New.java

---
+++ New.java
@@ -0,0 +1,7 @@
+package Java3;
+public class NewJava {
+    public static void main(String[] args) {
+        java3 person = new java3(10204,"роман","Отцы и дети",1860, "Русский вестник",200,"И.С.Тургенев");
+        System.out.println(person);
+    }
+}
